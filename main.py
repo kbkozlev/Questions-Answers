94915ce93b09a028d8bb1fc1e99182b412c2db57
Kaloian Kozlev <kbkozlev@gmail.com>
2021-12-04 20:41:53 +0200

    First

---
+++ main.py
@@ -0,0 +1,54 @@
+from openpyxl import load_workbook
+import random
+import os
+
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+wb = load_workbook("Questions&Answers.xlsx")  # Work Book
+ws = wb['Sheet1']  # Work Sheet
+questions = ws['A']  # Column
+answers = ws['B']
+questions_list = [questions[x].value for x in range(len(questions))]
+answers_list = [answers[x].value for x in range(len(answers))]
+
+rn = list(zip(questions_list, answers_list))
+random.shuffle(rn)
+questions_list, answers_list = zip(*rn)
+
+
+def calc():
+    number = 1
+    calc.points = 0
+    for x in range(len(questions_list)):
+        input((str(number) + '. ' + questions_list[x]))
+        number += 1
+        p = input('\n' + '- ' + answers_list[x] + "\n" + '''
+========================
+  Is this correct? Y/N ''')
+        if p == 'y':
+            calc.points += 1
+            cls()
+        else:
+            cls()
+            continue
+
+
+calc()
+
+x = True
+
+while x:
+
+    print("You got " + str(calc.points) + " points.\n")
+    cn = input("Continue?...y/n ")
+    cn.lower()
+    if cn == 'y':
+        random.shuffle(rn)
+        questions_list, answers_list = zip(*rn)
+        calc()
+    else:
+        x = False
+        break
